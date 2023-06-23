30db4d4ef676cb1aea35146c23516ce5b141ab0b
Exequiel <alvarado.exequiel@gmail.com>
2023-06-22 23:23:24 -0400

    update slug
    se agrega slug en models de la base de datos

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=0, default= 0) #12345678.12
+    slug = models.SlugField(null=False, blank=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
