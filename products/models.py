703b8a17dc9578b9223f7df0f82abf6290719554
Exequiel <alvarado.exequiel@gmail.com>
2023-06-23 10:15:21 -0400

    update slug unico
    generamos un scrip pra slug unico en la base de datos
    
     while Product.object.filter(slug=slug).exicts():
                slug = slugify(
                    '{}-{}'.format(instance.title, str(uuid.uuid4())[:8] )
                )

--- products/models.py
+++ products/models.py
@@ -1,3 +1,6 @@
+import uuid
+
+
 from django.db import models
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
@@ -19,7 +22,15 @@ class Product(models.Model):
         return self.title
 
 def set_slug(sender, instance, *args, **kwargs): #callback
-    instance.slug = slugify(instance.title)
+    if instance.title and not instance.slug:
+        slug = slugify(instance.title)
+
+        while Product.object.filter(slug=slug).exicts():
+            slug = slugify(
+                '{}-{}'.format(instance.title, str(uuid.uuid4())[:8] )
+            )
+
+        instance.slug = slug
 
 
 pre_save.connect(set_slug, sender=Product)
