cc79d0fb25dd311d0e5e97cb717ac576e02613c4
Exequiel <alvarado.exequiel@gmail.com>
2023-06-26 19:06:45 -0400

    update crud & add error 404
    se valida crud en carrito y se agrega la excepción 404

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect,render
+from django.shortcuts import get_object_or_404
 
-
-
+from products.models import Product
 from .models import Cart
 from .utils import get_or_create_cart
 
@@ -22,5 +22,24 @@ def cart(request):
     cart = get_or_create_cart(request)
 
     return render (request, 'carts/cart.html',{
+        'cart':cart
 
     })
+
+def add(request):
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+
+    cart.products.add(product )
+
+    return render (request, 'carts/add.html',{
+        'product': product
+    })
+def remove(request):
+    
+    cart = get_or_create_cart(request)
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
+
+    cart.products.remove(product)
+
+    return redirect ('carts:cart')
